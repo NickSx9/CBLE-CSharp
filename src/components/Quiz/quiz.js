bca8cd32b94af05ea07fbe55bc3cdf8299869a59
NickSx8 <nh580@student.anglia.ac.uk>
2021-03-06 19:25:47 +0000

    submit button for quiz now works

--- src/components/Quiz/quiz.js
+++ src/components/Quiz/quiz.js
@@ -1,3 +1,4 @@
+import { Alert } from 'bootstrap';
 import React from 'react';
 import RadioButton from '../RadioButtons/radiobutton'
 import './quiz.css';
@@ -24,8 +25,71 @@ export class Quiz extends React.Component {
       });
     }
   }
-  handelSubmit(){
-   console.log("Button Clicked")
+  handelSubmit = () => {
+   let correctCount = 0;
+   var answerQuestionOne = document.getElementById("questionOne_1").checked
+   var answerQuestionTwo = document.getElementById("questionTwo_3").checked
+   var answerQuestionThree = document.getElementById("questionThree_2").checked
+   var answerQuestionFour = document.getElementById("questionFour_4").checked
+   var answerQuestionFive = document.getElementById("questionFive_3").checked
+
+   if(answerQuestionOne)
+   {
+    correctCount++
+     document.getElementById("questionOne_1").className += " isCorrect"
+   }
+   else
+   {
+    document.getElementById("questionOne_1").className += " isCorrect"
+    document.getElementById("questionOne_2").className += " isIncorrect"
+    document.getElementById("questionOne_3").className += " isIncorrect"
+    document.getElementById("questionOne_4").className += " isIncorrect"
+   }
+   if(answerQuestionTwo)
+   {
+    correctCount++
+    document.getElementById("questionTwo_3").className += " isCorrect"
+   }
+   else{
+    document.getElementById("questionTwo_3").className += " isCorrect"
+    document.getElementById("questionTwo_2").className += " isIncorrect"
+    document.getElementById("questionTwo_1").className += " isIncorrect"
+    document.getElementById("questionTwo_4").className += " isIncorrect"
+   }
+   if(answerQuestionThree)
+   {
+    correctCount++
+    document.getElementById("questionThree_2").className += " isCorrect"
+   }
+   else{
+    document.getElementById("questionThree_2").className += " isCorrect"
+    document.getElementById("questionThree_3").className += " isIncorrect"
+    document.getElementById("questionThree_4").className += " isIncorrect"
+    document.getElementById("questionThree_1").className += " isIncorrect"
+   }
+   if(answerQuestionFour)
+   {
+    correctCount++
+    document.getElementById("questionFour_4").className += " isCorrect"
+   }
+   else{
+    document.getElementById("questionFour_4").className += " isCorrect"
+    document.getElementById("questionFour_3").className += " isIncorrect"
+    document.getElementById("questionFour_1").className += " isIncorrect"
+    document.getElementById("questionFour_2").className += " isIncorrect"
+   }
+   if(answerQuestionFive)
+   {
+    correctCount++
+    document.getElementById("questionFive_3").className += " isCorrect"
+   }
+   else{
+    document.getElementById("questionFive_3").className += " isCorrect"
+    document.getElementById("questionFive_1").className += " isIncorrect"
+    document.getElementById("questionFive_2").className += " isIncorrect"
+    document.getElementById("questionFive_4").className += " isIncorrect"
+   }
+   alert('You managed to get '+correctCount+' out of 5 correct.')
   }
   render () {  
       return (
@@ -45,7 +109,7 @@ export class Quiz extends React.Component {
                     </ul>
                 </div>
             </form>
-            <button type="submit" class="submitbutton">Submit</button>
+            <button type="submit" class="submitbutton" onClick={this.handelSubmit}>Submit</button>
         </div>
       );
     } 
