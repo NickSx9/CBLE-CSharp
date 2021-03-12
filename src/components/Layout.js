35bfa2160645ad1e559cb60a90f7e6b246cec1f4
NickSx8 <nh580@student.anglia.ac.uk>
2021-03-12 16:33:45 +0000

    need to change something so it would deploy correctly

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import { Container } from 'reactstrap';
 import { Header } from '../components/Header/Header.js';
 import SideBar  from '../components/SideBar/SideBar.js';
 import { ProgressBar } from '../components/ProgressBar/ProgressBar.js';
+import Home  from '../components/Home.js'
 
 
 export class Layout extends Component {
@@ -15,7 +16,7 @@ export class Layout extends Component {
            <Header />
            <div className="row">
               <div className="col-lg-9 col-md-9 col-sm-12 main-body">
-                {this.props.children}
+                  <Home/>
                 <ProgressBar/>
               </div>
             <div className="col-lg-3  col-md-3 col-sm-12 navigationSideBar">
