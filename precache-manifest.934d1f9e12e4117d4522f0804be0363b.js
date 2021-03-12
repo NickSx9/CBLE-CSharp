19b81c1d8d32adc1425202a78d3f9378352736b8
NickSx8 <nh580@student.anglia.ac.uk>
2021-03-12 15:58:02 +0000

    Updates

---
+++ precache-manifest.934d1f9e12e4117d4522f0804be0363b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98583014cd42fff07f1018e8e282e701",
+    "url": "/CBLE-CSharp/index.html"
+  },
+  {
+    "revision": "dddd772ec2bea9bb0586",
+    "url": "/CBLE-CSharp/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "f6ae385e17b49fdf23fa",
+    "url": "/CBLE-CSharp/static/css/main.deca950a.chunk.css"
+  },
+  {
+    "revision": "dddd772ec2bea9bb0586",
+    "url": "/CBLE-CSharp/static/js/2.ae8c62f0.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/CBLE-CSharp/static/js/2.ae8c62f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ae385e17b49fdf23fa",
+    "url": "/CBLE-CSharp/static/js/main.d3482d55.chunk.js"
+  },
+  {
+    "revision": "fc4e8189eb83aacbce14",
+    "url": "/CBLE-CSharp/static/js/runtime-main.8b9a56b8.js"
+  },
+  {
+    "revision": "5347d20fbe7677f462822add15f4f40c",
+    "url": "/CBLE-CSharp/static/media/classes.5347d20f.JPG"
+  },
+  {
+    "revision": "5a773009ce1183f45571c5ed3621ae26",
+    "url": "/CBLE-CSharp/static/media/first.5a773009.JPG"
+  },
+  {
+    "revision": "053684c49afafa4fa299a0a6ac5aedf2",
+    "url": "/CBLE-CSharp/static/media/hello.053684c4.JPG"
+  },
+  {
+    "revision": "00e7d4eab63ed7764af05b059bb9a2ed",
+    "url": "/CBLE-CSharp/static/media/intro.00e7d4ea.mp4"
+  },
+  {
+    "revision": "324413e020f1b68abc14c712506b0e7e",
+    "url": "/CBLE-CSharp/static/media/intro.324413e0.JPG"
+  },
+  {
+    "revision": "5af6bd5e14d5665a824b9dcec6b4701e",
+    "url": "/CBLE-CSharp/static/media/vari.5af6bd5e.JPG"
+  },
+  {
+    "revision": "eac7b9fe228c9da007ebdab007259fc1",
+    "url": "/CBLE-CSharp/static/media/vid2.eac7b9fe.mp4"
+  },
+  {
+    "revision": "7a55f7b2aef55770e5ea3526bd424df2",
+    "url": "/CBLE-CSharp/static/media/vid3.7a55f7b2.mp4"
+  },
+  {
+    "revision": "4cf6fedb86a126486925899c2e457325",
+    "url": "/CBLE-CSharp/static/media/vid4.4cf6fedb.mp4"
+  },
+  {
+    "revision": "8fd8371e48f5b23b94c7f03b745f70a9",
+    "url": "/CBLE-CSharp/static/media/vid5.8fd8371e.mp4"
+  }
+]);
